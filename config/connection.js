c1743b321519d8aaaf143a6fa2f2c29f4e222329
Tripp's Repo <tripp.parham@me.com>
2020-02-20 18:31:38 -0500

    Initial commit

---
+++ config/connection.js
@@ -0,0 +1,33 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+// DECLARING PORT 
+var PORT = process.env.PORT || 3306; 
+
+var connection;
+//ADD this link to interact with JAWSDB
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+
+      host: "localhost",
+      port: PORT,
+      user: "root",
+      password: "root",
+      database: "listing_db"
+    });
+};
+
+// Error connecting to JAWSDB or localhost
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS listing_db;
+CREATE DATABASE listing_db;
+USE listing_db;
+
+CREATE TABLE listing
+(
+	id int NOT NULL AUTO_INCREMENT,
+	keyword VARCHAR(100),
+	city VARCHAR(100),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE search
+(
+	id int NOT NULL AUTO_INCREMENT,
+    companyName VARCHAR(100),
+    title VARCHAR(100),
+	summary VARCHAR(100),
+	locality VARCHAR(100),
+    dateCreated VARCHAR(100),
+	PRIMARY KEY (id)
+);
