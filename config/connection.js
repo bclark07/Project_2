e4086a4a60384a38b886674dfb084925865c5ed1
Tripp's Repo <tripp.parham@me.com>
2020-02-20 19:28:30 -0500

    Lint fix

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// DECLARING PORT 
-var PORT = process.env.PORT || 3306; 
+//DECLARING PORT
+var PORT = process.env.PORT || 3306;
 
 var connection;
 //ADD this link to interact with JAWSDB
 if (process.env.JAWSDB_URL) {
- connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
 
-      host: "localhost",
-      port: PORT,
-      user: "root",
-      password: "root",
-      database: "listing_db"
-    });
-};
+    host: "localhost",
+    port: PORT,
+    user: "root",
+    password: "root",
+    database: "listing_db"
+  });
+}
 
 // Error connecting to JAWSDB or localhost
 connection.connect(function(err) {
