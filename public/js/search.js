33af324f09f697a943e7df2a8c6e73cbf748ebc2
Clint Gordy <gordyclint@gmail.com>
2020-02-23 23:16:52 -0500

    Finalizing the search functionality and adding tables to mysql database

--- public/js/search.js
+++ public/js/search.js
@@ -9,132 +9,131 @@
 // key, but good tip for next time!
 
 $(function() {
-    // On-click for starting the job search.
-    $("#searchBtn").on("click", function() {
-        console.log("Search has been clicked.");
+  // On-click for starting the job search.
+  $("#searchBtn").on("click", function() {
+    console.log("Search has been clicked.");
 
-        var keyword = $("#keywordSearch").val();
-        var location = $("#citySearch").val();
+    var keyword = $("#keywordSearch").val();
+    var location = $("#citySearch").val();
 
-        // This portion gets the API's information based off your search
-        if (keyword.length > 0 && location.length > 0) {
-            $.get("/api/search/" + keyword + "/" + location).then(function(
-                data
-            ) {
-                console.log(data);
+    // This portion gets the API's information based off your search
+    if (keyword.length > 0 && location.length > 0) {
+      $.get("/api/listings/" + keyword + "/" + location).then(function(data) {
+        console.log(data);
 
-                // For-Loop that dynamically adds data in tables using Materialize i.e. What you see after you hit search
-                for (var i = 0; i < data.length; i++) {
-                    var row = $("<tr>");
-                    var td = $("<td>");
-                    td.append(
-                        data[i].title +
-                            "<br>" +
-                            data[i].location +
-                            "<br>" +
-                            data[i].company +
-                            "<br>" +
-                            data[i].how_to_apply +
-                            "<br>" +
-                            data[i].created_at
-                    );
+        // For-Loop that dynamically adds data in tables using Materialize i.e. What you see after you hit search
+        for (var i = 0; i < data.length; i++) {
+          var row = $("<tr>");
+          var td = $("<td>");
+          td.append(
+            data[i].title +
+              "<br>" +
+              data[i].location +
+              "<br>" +
+              data[i].company +
+              "<br>" +
+              data[i].how_to_apply +
+              "<br>" +
+              data[i].created_at
+          );
 
-                    //   Dynamically created Save button and adding data attributes to them
-                    var button = $("<button>");
-                    button.text("Save");
-                    button.attr("class", "saveButton");
-                    button.attr("data-id", data[i].id);
-                    button.attr("data-index", i);
+          //   Dynamically created Save button and adding data attributes to them
+          var button = $("<button>");
+          button.text("Save");
+          button.attr("class", "saveBtn");
+          button.attr("data-id", data[i].id);
+          button.attr("data-index", i);
 
-                    //   Adds everything to table
-                    td.append(button);
-                    row.append(td);
-                    $("#results").append(row);
-                }
-
-                $("#numResults").html(data.length);
-
-                // Save button that saves data to our database
-                $(".saveButton").on("click", function() {
-                    var i = $(this).attr("data-index");
-                    console.log("Save has been clicked.");
-                    console.log(data[i]);
-                    var savedListing = {};
-                    savedListing.title = data[i].title;
-                    $.ajax("/api/search", {
-                        type: "POST",
-                        data: savedListing
-                    }).then(function() {
-                        console.log(
-                            savedListing + " has been stored in saved listings"
-                        );
-                        location.reload();
-                    });
-                });
-            });
+          //   Adds everything to table
+          td.append(button);
+          row.append(td);
+          $("#results").append(row);
         }
-        //   NOTE FROM CLINT: This is most of the code I wrote over, including the API. I'm leaving it here for now, but feel free to delete if we no longer need it.
-        // const id = $(this).data("id");
 
-        // var newSearch = {
-        //   keyword: keyword,
-        //   city: city
-        // };
-        // console.log(newSearch);
-        // //Save search to database
-        // $.ajax("/api/listing/" + id, {
-        //   type: "PUT",
-        //   data: newSearch
-        // }).then(function () {
-        //   console.log(newSearch + " has been stored in search history");
-        //   location.reload();
-        // });
+        $("#numResults").html(data.length);
 
-        // function listingPull (keyword, city) {
-        //   $.ajax({
-        //     url: "https://jobs.github.com/positions.json?description=javascript&location=new+york",
-        //     method: "GET"
-        //   }).then(function (response) {
-        //     console.log(response + "----testing");
-        //   });
-        // }
-        // listingPull();
-    });
+        // Save button that saves data to our database
+        $(".saveBtn").on("click", function() {
+          var i = $(this).attr("data-index");
+          console.log(data[i]);
+          var savedListing = {};
 
-    // CLICK HANDLER FOR ADDING A SAVED SEARCH
-    // DON'T DELETE: We still need this for saving the whole listing.
+          savedListing.company = data[i].company;
+          savedListing.location = data[i].location;
+          savedListing.title = data[i].title;
+          savedListing.howToApply = data[i].how_to_apply;
 
-    $(".saveListing").on("click", function() {
-        console.log("Save listing has been clicked.");
-
-        var id = $(this).data("id");
-        var companyName = $(this).data("companyName");
-        var title = $(this).data("title");
-        var summary = $(this).data("summary");
-        var locality = $(this).data("location");
-        var dateCreated = $(this).data("dateCreated");
-
-        var newListing = {
-            companyName: companyName,
-            title: title,
-            summary: summary,
-            locality: locality,
-            dateCreated: dateCreated
-        };
-        console.log(newListing);
-        //Save search to database
-        $.ajax("/api/listing/" + id, {
-            type: "PUT",
-            data: newListing
-        }).then(function() {
-            console.log(newListing + " has been stored in saved listings");
-            location.reload();
+          $.ajax("/api/saved-listings", {
+            type: "POST",
+            data: savedListing
+          }).then(function() {
+            console.log("Save has been clicked.");
+          });
         });
-    });
+      });
+    }
+    //   NOTE FROM CLINT: This is most of the code I wrote over, including the API. I'm leaving it here for now, but feel free to delete if we no longer need it.
+    // const id = $(this).data("id");
 
-    // CLICK HANDLER FOR ADDING A SAVED SEARCH
-    $(".logout").on("click", function() {
-        console.log("Logout has been clicked.");
-        // Logout needs to be coded
+    // var newSearch = {
+    //   keyword: keyword,
+    //   city: city
+    // };
+    // console.log(newSearch);
+    // //Save search to database
+    // $.ajax("/api/listing/" + id, {
+    //   type: "PUT",
+    //   data: newSearch
+    // }).then(function () {
+    //   console.log(newSearch + " has been stored in search history");
+    //   location.reload();
+    // });
+
+    // function listingPull (keyword, city) {
+    //   $.ajax({
+    //     url: "https://jobs.github.com/positions.json?description=javascript&location=new+york",
+    //     method: "GET"
+    //   }).then(function (response) {
+    //     console.log(response + "----testing");
+    //   });
+    // }
+    // listingPull();
+  });
+
+  // CLICK HANDLER FOR ADDING A SAVED SEARCH
+  // DON'T DELETE: We still need this for saving the whole listing.
+
+  $(".saveListing").on("click", function() {
+    console.log("Save listing has been clicked.");
+
+    var id = $(this).data("id");
+    var companyName = $(this).data("companyName");
+    var title = $(this).data("title");
+    var summary = $(this).data("summary");
+    var locality = $(this).data("location");
+    var dateCreated = $(this).data("dateCreated");
+
+    var newListing = {
+      companyName: companyName,
+      title: title,
+      summary: summary,
+      locality: locality,
+      dateCreated: dateCreated
+    };
+    console.log(newListing);
+    //Save search to database
+    $.ajax("/api/listing/" + id, {
+      type: "PUT",
+      data: newListing
+    }).then(function() {
+      console.log(newListing + " has been stored in saved listings");
+      location.reload();
     });
+  });
+
+  // CLICK HANDLER FOR ADDING A SAVED SEARCH
+  $(".logout").on("click", function() {
+    console.log("Logout has been clicked.");
+    // Logout needs to be coded
+  });
 });
--- routes/search-api-routes.js
+++ routes/search-api-routes.js
@@ -3,64 +3,69 @@ var db = require("../models");
 // NOTE FROM CLINT: Needed this npm package to call an API here. Make sure to install this or the app won't work :/
 var axios = require("axios");
 
-
 // NOTE FROM CLINT: My tutor told me that it's a lot safer to call the API within the search-api-routes folder. For example,if someone wanted to steal our API key, all they would need to do is open Inspect in Google. I know we don't have an API key, but good tip for next time!
 
-module.exports = function (app) {
+module.exports = function(app) {
+  // GitHub Job Post API Call
+  app.get("/api/listings/:keyword/:location", function(req, res) {
+    var keyword = req.params.keyword;
+    var location = req.params.location;
+    axios
+      .get(
+        "https://jobs.github.com/positions.json?description=" +
+          keyword +
+          "&location=" +
+          location
+      )
+      .then(function(response) {
+        res.json(response.data);
+      });
+  });
 
-    // GitHub Job Post API Call
-    app.get("/api/search/:keyword/:location", function(req, res) {
-        var keyword = req.params.keyword;
-        var location = req.params.location;
-        axios.get("https://jobs.github.com/positions.json?description=" + keyword + "&location=" + location).then(function (response) {
-              res.json(response.data);
-        });
+  // Finding all listings
+  app.get("/api/listings", function(req, res) {
+    db.Listings.findAll({}).then(function(data) {
+      res.json(data);
     });
+  });
 
-    // Finding all listings
-    app.get("/api/search", function (req, res) {
-        db.Search.findAll({}).then(function (dbSearch) {
-            res.json(dbSearch);
-        });
+  // Finding one search based off ID
+  app.get("/api/listings:id", function(req, res) {
+    db.Listings.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(data) {
+      res.json(data);
     });
+  });
 
-    // Finding one search based off ID
-    app.get("/api/search:id", function (req, res) {
-        db.Search.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbSearch) {
-            res.json(dbSearch);
-        });
+  // Route to save our data to the listing page
+  app.post("/api/saved-listings", function(req, res) {
+    db.Listings.create(req.body).then(function(data) {
+      res.json(data);
     });
+  });
 
-    // Route to save our data to the listing page
-    app.post("/api/search", function (req, res) {
-        db.Search.create(req.body).then(function (dbSearch) {
-            res.json(dbSearch);
-        });
+  // Delete Route
+  app.delete("/api/listings/:id", function(req, res) {
+    db.Listings.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(data) {
+      res.json(data);
     });
+  });
 
-    // Delete Route
-    app.delete("/api/search/:id", function (req, res) {
-        db.Search.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbSearch) {
-            res.json(dbSearch);
-        });
+  // Put route for updating posts
+  app.put("/api/listings", function(req, res) {
+    db.Listings.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(data) {
+      res.json(data);
     });
-
-    // Put route for updating posts
-    app.put("/api/search", function (req, res) {
-        db.Search.update(req.body, {
-            where: {
-                id: req.body.id
-            }
-        }).then(function (dbSearch) {
-            res.json(dbSearch);
-        });
-    });
+  });
 };
