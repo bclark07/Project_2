ae2cdf86d5240f58dad50686a69ecd8310938f05
Brittany Clark <clark.brittany.j@gmail.com>
2020-02-25 15:12:00 -0500

    pulls from listings DB to generate html on load and when click save button

--- public/js/search.js
+++ public/js/search.js
@@ -8,9 +8,37 @@
 //  if someone wanted to steal our API key, all they would need to do is open Inspect in Google. I know we don't have an API
 // key, but good tip for next time!
 
+// This function grabs posts from the database and updates the view
+
+function getSaved() {
+  $.get("/api/listings", function(data) {
+    console.log("Listings: ", data);
+    listings = data;
+    if (!listings || !listings.length) {
+      displayEmpty();
+    } else {
+      initializeRows(listings);
+    }
+  });
+}
+
+function initializeRows(listings) {
+  // blogContainer.empty(); should be used to empty search container
+  var savedJobs = [];
+  for (var i = 0; i < listings.length; i++) {
+    console.log("saved listing" + i + ";" + listings[i]);
+    savedJobs.push(CreateSavedEntry(listings[i]));
+  }
+  // blogContainer.append(savedJobs);
+}
+
 $(function() {
+  //on page load want to populate html with already saved jobs
+  getSaved();
+
   // On-click for starting the job search.
   $("#searchBtn").on("click", function() {
+    //put in code to clear previous search results
     console.log("Search has been clicked.");
 
     var keyword = $("#keywordSearch").val();
@@ -20,6 +48,7 @@ $(function() {
     if (keyword.length > 0 && location.length > 0) {
       $.get("/api/listings/" + keyword + "/" + location).then(function(data) {
         console.log(data);
+        //returns json response with job results
 
         // For-Loop that dynamically adds data in tables using Materialize i.e. What you see after you hit search
         for (var i = 0; i < data.length; i++) {
@@ -68,72 +97,108 @@ $(function() {
             data: savedListing
           }).then(function() {
             console.log("Save has been clicked.");
+            CreateSavedEntry(savedListing);
           });
         });
       });
     }
-    //   NOTE FROM CLINT: This is most of the code I wrote over, including the API. I'm leaving it here for now, but feel free to delete if we no longer need it.
-    // const id = $(this).data("id");
-
-    // var newSearch = {
-    //   keyword: keyword,
-    //   city: city
-    // };
-    // console.log(newSearch);
-    // //Save search to database
-    // $.ajax("/api/listing/" + id, {
-    //   type: "PUT",
-    //   data: newSearch
-    // }).then(function () {
-    //   console.log(newSearch + " has been stored in search history");
-    //   location.reload();
-    // });
-
-    // function listingPull (keyword, city) {
-    //   $.ajax({
-    //     url: "https://jobs.github.com/positions.json?description=javascript&location=new+york",
-    //     method: "GET"
-    //   }).then(function (response) {
-    //     console.log(response + "----testing");
-    //   });
-    // }
-    // listingPull();
-  });
-
-  // CLICK HANDLER FOR ADDING A SAVED SEARCH
-  // DON'T DELETE: We still need this for saving the whole listing.
-
-  $(".saveListing").on("click", function() {
-    console.log("Save listing has been clicked.");
-
-    var id = $(this).data("id");
-    var companyName = $(this).data("companyName");
-    var title = $(this).data("title");
-    var summary = $(this).data("summary");
-    var locality = $(this).data("location");
-    var dateCreated = $(this).data("dateCreated");
-
-    var newListing = {
-      companyName: companyName,
-      title: title,
-      summary: summary,
-      locality: locality,
-      dateCreated: dateCreated
-    };
-    console.log(newListing);
-    //Save search to database
-    $.ajax("/api/listing/" + id, {
-      type: "PUT",
-      data: newListing
-    }).then(function() {
-      console.log(newListing + " has been stored in saved listings");
-      location.reload();
-    });
-  });
-
-  // CLICK HANDLER FOR ADDING A SAVED SEARCH
-  $(".logout").on("click", function() {
-    console.log("Logout has been clicked.");
-    // Logout needs to be coded
   });
 });
+
+function CreateSavedEntry(data) {
+  //write saved jobs to the viewpane
+  var faveRow = $("<tr>");
+  var faveTd = $("<td>");
+  faveTd.append(
+    data.title + //data[i].title +
+    "<br>" +
+    data.location + //data[i].location +
+    "<br>" +
+    data.company + // data[i].company +
+      "<br>" +
+      data.howToApply //+ // data[i].how_to_apply +
+    // "<br>" +
+    // data[i].created_at
+  );
+
+  //   Dynamically created Delete button and adding data attributes to them
+  var button = $("<button>");
+  button.text("Delete");
+  button.attr("class", "deleteBtn");
+  button.attr("data-id", data.id);
+  // button.attr("data-index", i);
+
+  //   Adds everything to table
+  faveTd.append(button);
+  faveRow.append(faveTd);
+  $("#saved").append(faveRow);
+}
+
+// NOTE FROM CLINT: This is most of the code I wrote over, including the API. I'm leaving it here for now, but feel free to delete if we no longer need it.
+//BRITTANY THINKS IT CAN BE DELETED AS WELL
+// const id = $(this).data("id");
+
+// var newSearch = {
+//   keyword: keyword,
+//   city: city
+// };
+// console.log(newSearch);
+// //Save search to database
+// $.ajax("/api/listing/" + id, {
+//   type: "PUT",
+//   data: newSearch
+// }).then(function() {
+//   console.log(newSearch + " has been stored in search history");
+//   location.reload();
+// });
+
+//   function listingPull(keyword, city) {
+//     $.ajax({
+//       url:
+//         "https://jobs.github.com/positions.json?description=javascript&location=new+york",
+//       method: "GET"
+//     }).then(function(response) {
+//       console.log(response + "----testing");
+//     });
+//   }
+//   listingPull();
+// });
+
+// CLICK HANDLER FOR ADDING A SAVED SEARCH
+// DON'T DELETE: We still need this for saving the whole listing.
+//NOTE FROM BRITTANY: I think we can delete this - it is all embeded in the code above
+
+//   $(".saveListing").on("click", function() {
+//     console.log("Save listing has been clicked.");
+
+//     var id = $(this).data("id");
+//     var companyName = $(this).data("companyName");
+//     var title = $(this).data("title");
+//     var summary = $(this).data("summary");
+//     var locality = $(this).data("location");
+//     var dateCreated = $(this).data("dateCreated");
+
+//     var newListing = {
+//       companyName: companyName,
+//       title: title,
+//       summary: summary,
+//       locality: locality,
+//       dateCreated: dateCreated
+//     };
+//     console.log(newListing);
+//     //Save search to database
+//     $.ajax("/api/listing/" + id, {
+//       type: "PUT",
+//       data: newListing
+//     }).then(function() {
+//       console.log(newListing + " has been stored in saved listings");
+//       location.reload();
+//     });
+//   });
+
+//   // CLICK HANDLER FOR ADDING A SAVED SEARCH
+//   $(".logout").on("click", function() {
+//     console.log("Logout has been clicked.");
+//     // Logout needs to be coded
+//   });
+// });
